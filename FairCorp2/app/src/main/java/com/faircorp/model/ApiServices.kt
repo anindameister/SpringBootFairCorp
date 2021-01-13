2effc1d6cc935ea3a61c1d55e858294e1ba24250
anindamaulik <anindameister0@gmail.com>
2021-01-13 23:46:52 +0100

    using https://dev-mind.fr/training/android/ and making sure everything is perfect with android

--- FairCorp2/app/src/main/java/com/faircorp/model/ApiServices.kt
+++ FairCorp2/app/src/main/java/com/faircorp/model/ApiServices.kt
@@ -7,7 +7,7 @@ class ApiServices {
     val windowsApiService : WindowApiService by lazy {
         Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create())
-            .baseUrl("https://app-420ce9e6-69f1-4afa-8c93-613ae8951e47.cleverapps.io/api/")
+            .baseUrl("https://dev-mind.fr/training/android/")
             .build()
             .create(WindowApiService::class.java)
     }
